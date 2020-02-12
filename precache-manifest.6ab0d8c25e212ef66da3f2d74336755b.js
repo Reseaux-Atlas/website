7d70ac7ccfb3adf12fa28a73938bfae288e09100
tperale <thomas.perale@openmailbox.org>
2020-02-12 21:26:04 +0100

    Updates

---
+++ precache-manifest.6ab0d8c25e212ef66da3f2d74336755b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c4bc2560a32c3f50ccaef0c331f2cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d6af76705a08c6a02ee",
+    "url": "/static/css/2.0a4410c4.chunk.css"
+  },
+  {
+    "revision": "9a248d3ccd588f6887c8",
+    "url": "/static/css/main.da815385.chunk.css"
+  },
+  {
+    "revision": "5d6af76705a08c6a02ee",
+    "url": "/static/js/2.ac231d4e.chunk.js"
+  },
+  {
+    "revision": "c252fe9cf1ed82d21a64fae0719cd036",
+    "url": "/static/js/2.ac231d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a248d3ccd588f6887c8",
+    "url": "/static/js/main.281129f2.chunk.js"
+  },
+  {
+    "revision": "6f2af5780dcffaacca89",
+    "url": "/static/js/runtime-main.0b36d3b8.js"
+  },
+  {
+    "revision": "9fb66fd62eb570e1e25e867e713be75c",
+    "url": "/static/media/banner.9fb66fd6.png"
+  },
+  {
+    "revision": "0e2114d6e8cbf4cb9615d46721543991",
+    "url": "/static/media/marker.0e2114d6.png"
+  },
+  {
+    "revision": "278027af86a7dca8a51486bc64569814",
+    "url": "/static/media/mdr.278027af.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
